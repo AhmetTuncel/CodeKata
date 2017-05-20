a449666d82785d732aee0ca61da04af08d01be4c
AhmetTuncel <ahmet.tuncel@outlook.com>
2017-05-20 22:08:49 +0300

    Sum all the numbers of the array  except the highest and the lowest element (the value, not the index!). (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)

--- CodeKata/Sum_without_highest_and_lowest_number_Kata.py
+++ CodeKata/Sum_without_highest_and_lowest_number_Kata.py
@@ -1,6 +1,6 @@
 """
 
-Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+Sum all the numbers of the array  except the highest and the lowest element (the value, not the index!).
 (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
 
 """
