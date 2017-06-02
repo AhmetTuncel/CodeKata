c3ec205e3b6f438ac102cd90065fd841a480d49d
AhmetTuncel <ahmet.tuncel@outlook.com>
2017-06-02 10:24:42 +0300

    Make a function that can take any non-negative integer as a argument and return it with it's digits in descending order. Essentially, rearrange the digits to create the highest possible number

---
+++ CodeKata/7_Rank/Descending_Order_Kata.py
@@ -0,0 +1,7 @@
+"""
+Make a function that can take any non-negative integer as a argument and return it with it's digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+"""
+
+def Descending_Order(num):
+    x = int("".join(sorted(str(num), reverse = True)))
+    return x
