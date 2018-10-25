d435dc2f0c4101cf69931102d8e424f868a675b5
AhmetTuncel <ahmet.tuncel@outlook.com>
2018-10-25 13:22:51 +0300

    v

---
+++ CodeKata/6_Rank/Persistent_Bugger..py
@@ -0,0 +1,15 @@
+"""
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit. 
+"""
+from functools import reduce
+import operator
+
+def persistence(n):
+    if len(str(n)) == 1:
+        return 0
+    else:
+      val = reduce(operator.mul, map(int, str(n)))
+      x = 1 + persistence(val)
+    return x
+
+#print (accum("ZpglnRxqenU"))
